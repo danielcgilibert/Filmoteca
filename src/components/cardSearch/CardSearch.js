3aac0f7afed37ea1ddd13e68bc026e8bfee9a288
danielcgilibert <danielcgilibert@gmail.com>
2021-07-12 21:55:23 +0200

    diseñando tarjeta search

--- src/components/cardSearch/CardSearch.js
+++ src/components/cardSearch/CardSearch.js
@@ -1,26 +1,31 @@
-import React from 'react'
+import React from "react";
 
-export const CardSearch = ({movieSearch}) => {
+export const CardSearch = ({ movieSearch }) => {
   console.log(movieSearch);
-    const { title,poster_path,vote_average,overview} = movieSearch;
+  const { title, poster_path, vote_average, overview } = movieSearch;
 
-    return (
-        <div class="cardSearch mb-3">
-  <div class="row g-0">
-    
-    <div class="col-md-4 imageSearch" style={{backgroundImage: `url(https://image.tmdb.org/t/p/original//${poster_path})`  }}>
-    
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">{title}</h5>
-        <p class="card-text">{overview} </p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  return (
+    <div class="cardSearch mb-3">
+      <div class="row g-0">
+        <div
+          class="col-md-4 imageSearch"
+          style={{
+            backgroundImage: `url(https://image.tmdb.org/t/p/original//${poster_path})`,
+          }}
+        ></div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title"><strong> {title} </strong></h5>
+            <p class="card-text">{overview} </p>
+            <p class="card-text">
+              <small class="badge bg-primary text-wrap px-4 align-center fs-5">
+                <i class="bi bi-award mr-2"></i>
+                {vote_average}
+              </small>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-
-  
-    );
-}
+  );
+};
