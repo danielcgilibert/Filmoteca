e9a21f070aff98ce05a25e56e87b4064c054f4c7
Daniel <danielcgilibert@gmail.com>
2021-07-18 16:07:56 +0200

    carousel agregado

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -5,7 +5,9 @@ import { ListMovies } from "../components/listMovies/ListMovies";
 import { NavBar } from "../components/navbar/NavBar";
 
 export const HomeScreen = () => {
-  const { pageMoviesHome, moviesHome,upcomingMovies} = useSelector((state) => state.mv);
+  const { pageMoviesHome, moviesHome, upcomingMovies } = useSelector(
+    (state) => state.mv
+  );
   const { loading } = useSelector((state) => state.ui);
 
   const dispatch = useDispatch();
@@ -29,18 +31,85 @@ export const HomeScreen = () => {
   return (
     <>
       <NavBar />
-      <div className="container">
+      <div className="container ">
         <div className="jumbotron">
           <h1 className="display-4 mt-5">Bienvenido a Filmoteca</h1>
-          <hr className="my-4" />
-          <h2>Mejor Valoradas</h2>
         </div>
+
+        <div class="row  d-flex justify-content-center">
+          <div className="col-md-12 ">
+            <div
+              id="carouselExampleIndicators"
+              class="carousel slide "
+              data-ride="carousel"
+            >
+              <ol class="carousel-indicators">
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="0"
+                  class="active"
+                ></li>
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="1"
+                ></li>
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="2"
+                ></li>
+              </ol>
+              <div class="carousel-inner">
+                {moviesHome.map((movie,index) => {
+                  return (
+                    <>
+                    
+                      <div className={`carousel-item ${index===0?"active":""}`}>
+                        <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt="..." />
+                        <div class="carousel-caption d-none d-md-block">
+                          <h5>{movie.title} </h5>
+                        </div>
+                      </div>
+                    </>
+                  );
+                })}
+              </div>
+              <a
+                class="carousel-control-prev"
+                href="#carouselExampleIndicators"
+                role="button"
+                data-slide="prev"
+              >
+                <span
+                  class="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a
+                class="carousel-control-next"
+                href="#carouselExampleIndicators"
+                role="button"
+                data-slide="next"
+              >
+                <span
+                  class="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <hr className="my-4" />
+        <h2>Mejor Valoradas</h2>
+
         <div className="row">
           {loading ? (
             <div className="col-md-12 d-flex justify-content-center p-3">
-                <div className="spinner-border" role="status">
-                  <span className="visually-hidden">Loading...</span>
-                </div>
+              <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </div>
             </div>
           ) : (
             <>
@@ -58,27 +127,24 @@ export const HomeScreen = () => {
           )}
         </div>
 
-            <hr/>
+        <hr />
 
-            <div className="row">
-        <h2>Próximas películas  </h2>
+        <div className="row">
+          <h2>Próximas películas </h2>
+          <>
+            <div className="d-flex flex-row-reverse mt-3">
+              <button onClick={handleScroll} className="btn">
+                <i className="bi bi-arrow-down botonAbajo"></i>
+              </button>
+            </div>
 
-          
-            <>
-              <div className="d-flex flex-row-reverse mt-3">
-                <button onClick={handleScroll} className="btn">
-                  <i className="bi bi-arrow-down botonAbajo"></i>
-                </button>
-              </div>
-
-              <ListMovies moviesHome={upcomingMovies} />
-              {/* <button onClick={handleNextPage} className="login__input mt-3">
+            <ListMovies moviesHome={upcomingMovies} />
+            {/* <button onClick={handleNextPage} className="login__input mt-3">
                 <i className="bi bi-arrow-down"></i>
               </button> */}
-            </>
+          </>
           )
         </div>
-
       </div>
     </>
   );
